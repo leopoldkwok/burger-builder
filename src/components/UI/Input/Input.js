e227f020adbc42916aa39bb9e34b09579872ad5f
leopoldkwok <leopoldkwok@gmail.com>
2020-05-22 23:42:51 +0100

    Handling Form Submission

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -9,26 +9,30 @@ const input = (props) => {
     case 'input':
       inputElement = (
         <input
-          className={classes.inputElement}
+          className={classes.InputElement}
           {...props.elementConfig}
           value={props.value}
+          onChange={props.changed}
         />
       );
       break;
     case 'textarea':
       inputElement = (
         <textarea
-          className={classes.inputElement}
+          className={classes.InputElement}
           {...props.elementConfig}
           value={props.value}
+          onChange={props.changed}
         />
       );
       break;
     case 'select':
       inputElement = (
-        <select>
-          className={classes.inputElement}
+        <select
+          className={classes.InputElement}
           value={props.value}
+          onChange={props.changed}
+        >
           {props.elementConfig.options.map((option) => (
             <option key={option.value} value={option.value}>
               {option.displayValue}
@@ -40,9 +44,10 @@ const input = (props) => {
     default:
       inputElement = (
         <input
-          className={classes.inputElement}
+          className={classes.InputElement}
           {...props.elementConfig}
           value={props.value}
+          onChange={props.changed}
         />
       );
   }
